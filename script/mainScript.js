386ed59080a20a83786d7d275fd148340725a51b
Yanick <yayey.christen@gmail.com>
2021-03-23 16:20:06 +0100

    added Main Styles

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,9 +1,14 @@
 playBtn = document.getElementById("playBtn");
 titleDiv = document.getElementById("titleDiv");
 backToTitleBtn = document.getElementById("backToTitleBtn");
+titleAnimationTime = 2;
 
 playBtn.addEventListener("click", () => {
-    titleDiv.style.visibility = "hidden";
+    titleDiv.style.animation = `fadeOutAnimation ${titleAnimationTime}s`
+    setTimeout(function() {
+        titleDiv.style.visibility = "hidden";
+       },titleAnimationTime * 1000);
+    
 })
 
 backToTitleBtn.addEventListener("click", () => {
